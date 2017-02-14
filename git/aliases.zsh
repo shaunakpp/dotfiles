5205a052cb5b23f3ffd0c8e7a176e095db610d8b
shaunakpp <shaunak.pagnis@gmail.com>
2017-02-14 12:09:35 +0530

    modified git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,5 +19,6 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias ga='git add'
+alias gst='git status' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
